88e75570400cec85535231acd3c41740f6726713
H.O <honoka.okabe7@gmal.com>
2019-03-26 11:36:58 +0900

    implement reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,4 @@
+import {combineReducers} from "redux"
+import count from './count'
 
-export const increment = () => ({
-    type: 'INCREMENT'
-})
-
-export const decrement = () => ({
-    type: 'DECREMENT'
-})
-
+export default combineReducers({count})
