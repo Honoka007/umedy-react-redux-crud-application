9e695aa0cee9c9c346c456c71e50061b51fbbaae
岡部 帆乃香 <hookabe@crooz.co.jp>
2019-04-24 12:29:44 +0900

    inplement new component event-new

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,27 +1,66 @@
 // eslint-disable-next-line
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom'
 
-// import { postEvent } from '../actions'
+import { postEvent } from '../actions'
 
 
 class EventsNew extends Component {
+    constructor(props) {
+        super(props)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+    renderField(field){
+        // eslint-disable-next-line
+        const {input, label, type, meta:{touched,error}} = field
+        return(
+        <div>
+            <input {...input} placeholder={label} type={type}/>
+            {touched && error && <span>{error}</span>}
+        </div>
+        )
+    }
 
-    render(){// eslint-disable-next-line
-        const props = this.props
+    async onSubmit(values){
+        await this.props.postEvent(values)
+        this.props.history.push('/')
+    }
+
+    render(){
+        const { handleSubmit } = this.props
 
         return (
-            <React.Fragment>
-                <div>foo</div>
-            </React.Fragment>
+            <form onSubmit={handleSubmit(this.onSubmit)}>
+                <div>
+                    <Field label="Title" name="title" type="text" component={this.renderField}></Field>
+                </div>
+                <div>
+                    <Field label="Body" name="body" type="text" component={this.renderField}></Field>
+                </div>
+                <div>
+                    <input type="submit" value="Submit" disabled={false}></input>
+                    <Link to="/">Cancel</Link>
+                </div>
+            </form>
         )
     }
 
 }
 
 
-// const mapDispatchToProps = ({postEvents})
+const validate = values => {
+    const errors = {}
+        if(!values.title) errors.title = "Enter a title, please."
+        if (!values.body) errors.body = "Enter a body, please."
+    return errors
+}
 
-export default connect(null, null)(EventsNew);
+const mapDispatchToProps = ({postEvent})
+
+export default connect(null, mapDispatchToProps)(
+    reduxForm({validate, form: 'eventNewForm'})
+    (EventsNew)
+)
 
