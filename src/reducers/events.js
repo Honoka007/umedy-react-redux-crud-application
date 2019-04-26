a152694fface70af26761def7e03d7233789ebe9
岡部 帆乃香 <hookabe@crooz.co.jp>
2019-04-26 16:28:06 +0900

    block submit

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,13 +1,27 @@
 // lodash.js 配列を欲しい形に整形する
 // 今回はmapKey仕様でkeyを作成
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import {
+    CREATE_EVENTS,
+    READ_EVENTS,
+    READ_EVENT,
+    UPDATE_EVENT,
+    DELETE_EVENT,
+} from '../actions'
 
-export default (state = {}, action) => {
+export default (events = {}, action) => {
     switch (action.type){
+        case CREATE_EVENTS:
+        case READ_EVENT:
+        case UPDATE_EVENT:
+            const data = action.response.data
+            return { ...events, [data.id]: data }
         case READ_EVENTS:
             return (_.mapKeys(action.response.data, 'id'))
+        case DELETE_EVENT:
+            delete events[action.id]
+            return { ...events }
         default:
-            return state
+            return events
     }
 }
